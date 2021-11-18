56382ac084f8a7450c8b421d02cab611eac02bbd
ndukwesamuel <ndukwesamuel23@yahoo.comm>
2021-11-18 12:28:11 +0100

    new changes

--- form/views.py
+++ form/views.py
@@ -1,24 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from form.forms import myModelForm
+from .forms import myModelForm
 
 def form(request):
 
     return render(request, "form.html")
 
+
+
 def get_form(request):
-    # if request.method == 'POST':
-    
     form = myModelForm(request.POST or None)
     if form.is_valid():
         form.save()
 
-
-    form = "sam"
-    context = {
-        'form': form
-    }
-
-    return render(request , 'form.html', context)
+    context =  {'form':form}
+    return render(request, 'test.html',context )
             
